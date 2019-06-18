45f7a3ade7addaf9040c5307cc2d4db5cadd3d2d
Helen <abedul@tut.by>
2019-06-18 14:34:34 +0300

    stylistic corrections

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @favorites = Note.where(user_id: params[:id], favorite: 1).paginate(page: params[:page])
+    @favorites = Note.where(user_id: params[:id], favorite: 1)
+                     .paginate(page: params[:page])
   end
 
   def new
