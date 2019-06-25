1c22b8dd892a82e82dad6dba72a1f93df000c981
Helen <abedul@tut.by>
2019-06-25 10:42:55 +0300

    stylistic correction

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -14,9 +14,10 @@ RSpec.describe Category, type: :model do
   describe 'associations' do
     it { should belong_to(:user) }
     it { should belong_to(:parent).class_name('Category').optional }
-    it { should have_many(:subcategories).class_name('Category')
-                                         .with_foreign_key('parent_id')
-                                         .dependent(:destroy) }
+    it do
+      should have_many(:subcategories).class_name('Category')
+        .with_foreign_key('parent_id').dependent(:destroy)
+    end
     it { should have_many(:notes).dependent(:destroy) }
   end
 
@@ -25,10 +26,12 @@ RSpec.describe Category, type: :model do
     it { should have_db_column(:favorite) }
   end
 
-  context 'methods' do  
+  context 'methods' do
     describe 'nested_notes_count' do
       context 'if there are not any nested notes' do
-        subject { create(:category_with_subcategories, user: user) }
+        subject do
+          create(:category_with_subcategories, user: user)
+        end
 
         it 'should return 0' do
           expect(subject.nested_notes_count).to eq(0)
@@ -38,7 +41,10 @@ RSpec.describe Category, type: :model do
       context 'if there are nested notes in subcategories' do
         let(:subs_count) { 2 }
         let(:notes_count) { 3 }
-        subject { create(:category_with_subcategories_and_notes, subs_count: subs_count, notes_count: notes_count) }
+        subject do
+          create(:category_with_subcategories_and_notes,
+                 subs_count: subs_count, notes_count: notes_count)
+        end
 
         it 'should return count of all notes in all subcategories' do
           expect(subject.nested_notes_count).to eq(notes_count * subs_count)
@@ -46,25 +52,27 @@ RSpec.describe Category, type: :model do
 
         context 'when a note is deleted' do
           it 'should decrement nested notes count' do
-            expect { subject.subcategories.first.notes.first.destroy }.to change { subject.nested_notes_count }.by(-1)
+            expect { subject.subcategories.first.notes.first.destroy }.to
+                      change { subject.nested_notes_count }.by(-1)
           end
         end
 
         context 'when a new note is added' do
-          it 'should increment nested notes count' do           
-            expect { subject.subcategories.first.notes.create!(title: 'note', user_id: user.id) }
-                         .to change { subject.nested_notes_count }.by(1)
+          it 'should increment nested notes count' do      
+            expect { subject.subcategories.first.notes.create!(title: 'note',
+                                                               user_id: user.id) }
+                      .to change { subject.nested_notes_count }.by(1)
           end
         end
-      end 
+      end
     end
- 
+
     describe 'update_related_items' do
       context 'when favorite token value is' do
         context '0' do
           let(:favorite_token) { 0 }
           include_context 'initialize subject'
-        
+
           context 'all related items favorite status' do
             include_context 'one category favorite status updated'
             include_examples 'favorite status of related items should be changed'
@@ -73,11 +81,11 @@ RSpec.describe Category, type: :model do
 
         context '1' do
           let(:favorite_token) { 1 }
-        
+
           context 'all nested items favorite status' do
             include_context 'initialize subject'
             include_context 'one category favorite status updated'
-            
+
             include_examples 'favorite status of related items should be changed'
           end
 
@@ -101,15 +109,19 @@ RSpec.describe Category, type: :model do
       end
 
       context 'if  category contains subcategories' do
-        subject { create(:category_with_two_levels_subcategories, subs_count: 2, subs_2_count: 2) }
+        subject do
+          create(:category_with_two_levels_subcategories, subs_count: 2,
+                 subs_2_count: 2)
+        end
 
         it 'should return the category with all subcategories' do
-          expect(subject.each).to contain_exactly(subject, subject.subcategories.first,
-                                                  subject.subcategories.second, 
-                                                  subject.subcategories.first.subcategories.first,
-                                                  subject.subcategories.first.subcategories.second,
-                                                  subject.subcategories.second.subcategories.first,
-                                                  subject.subcategories.second.subcategories.second)
+          expect(subject.each).to
+            contain_exactly(subject, subject.subcategories.first,
+                            subject.subcategories.second,
+                            subject.subcategories.first.subcategories.first,
+                            subject.subcategories.first.subcategories.second,
+                            subject.subcategories.second.subcategories.first,
+                            subject.subcategories.second.subcategories.second)
         end
       end
     end
