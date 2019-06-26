dc76057154a7c267f4e4ce03e63212cc8c6198f8
Helen <abedul@tut.by>
2019-06-26 12:35:38 +0300

    categories controller spec

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -4,23 +4,24 @@ RSpec.describe CategoriesController, type: :controller do
   context 'when user is not logged in' do
     describe 'POST #create' do
       let(:category_params) { { name: 'Music', favorite_token: 0 } }
-      let(:create_category) do
-        post :create, category: category_params
+      let(:post_create) do
+        post :create, params: { category: category_params }
       end
 
       it 'should redirect to login url' do
-        expect { create_category }.to { avoid_changing { Category.all.count } }
+        expect { post_create }.not_to change { Category.all.count }
         should redirect_to(login_url)
       end
     end
 
     describe 'DELETE #destroy' do
+      before(:example) { @category = create(:category) }
       let(:delete_category) do
-        delete :destroy, id: create(:category).id
+        delete :destroy, params: { :id => @category.id }
       end
 
-      it 'should redirect to login url' do
-        expect { delete_category }.to { avoid_changing { Category.all.count } }
+      it 'should redirect to login url' do       
+        expect { delete_category }.not_to change { Category.all.count }
         should redirect_to(login_url)
       end
     end
