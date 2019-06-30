c8daaf229e77b0874205f875ebe1b60c88f90d8d
egterent <abedul@tut.by>
2019-06-30 03:23:29 +0300

    rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,12 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'devise'
 require 'capybara/rails'
+require 'dotenv/load'
+require 'carrierwave'
+require 'fog'
+require_relative 'support/controller_macros'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -61,7 +66,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
+  config.include Devise::Test::ControllerHelpers, :type => :controller
+  
   config.include FactoryBot::Syntax::Methods
 end
 
