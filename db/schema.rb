3fa456766d49732d1ee5075404e54ea73b21a2a7
egterent <abedul@tut.by>
2019-06-30 11:39:03 +0300

    db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_18_131408) do
+ActiveRecord::Schema.define(version: 2019_06_25_032249) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_06_18_131408) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
