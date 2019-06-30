6447e60a03e284fb0a7b732c4f29ef54e1b8c844
egterent <abedul@tut.by>
2019-06-30 04:45:18 +0300

    stylistic correction

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,14 +1,14 @@
 module ControllerMacros
   def login_admin
     before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:admin]
+      @request.env['devise.mapping'] = Devise.mappings[:admin]
       sign_in FactoryBot.create(:user, admin: true)
     end
   end
 
   def login_user
     before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:user]
+      @request.env['devise.mapping'] = Devise.mappings[:user]
       user = FactoryBot.create(:user)
       sign_in user
     end
